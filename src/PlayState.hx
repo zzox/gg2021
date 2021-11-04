e04197488dd4140d6eda2821b2eea191fe1c448f
Tyler McCarthy <tweedlitweedli@gmail.com>
2021-11-03 22:23:05 -0500

    test map with movement

--- src/PlayState.hx
+++ src/PlayState.hx
@@ -1,18 +1,60 @@
 package;
 
+import flixel.FlxG;
 import flixel.FlxState;
+import flixel.addons.editors.tiled.TiledMap;
+import flixel.addons.editors.tiled.TiledTileLayer;
+import flixel.tile.FlxBaseTilemap.FlxTilemapAutoTiling;
+import flixel.tile.FlxTilemap;
 import objects.Player;
 
 class PlayState extends FlxState {
+    static inline final Y_MAP_OFFSET = -8;
+
+    var groundLayer:FlxTilemap;
+    var player:Player;
+
     override public function create() {
         super.create();
 
         camera.pixelPerfectRender = true;
 
-        add(new Player(160, 16, this));
+        // MD:
+        final map = new TiledMap(AssetPaths.soar_test__tmx);
+        groundLayer = createTileLayer(map, 'ground');
+        add(groundLayer);
+
+        player = new Player(160, 16, this);
+        add(player);
+
+        // MD:
+        FlxG.worldBounds.set(0, 0, 640, 360);
+        FlxG.camera.setScrollBounds(0, 640, 0, 360);
+
+        FlxG.camera.follow(player);
+
+        bgColor = 0xff98dcff;
     }
 
     override public function update(elapsed:Float) {
         super.update(elapsed);
+
+        FlxG.collide(groundLayer, player);
+    }
+
+    function createTileLayer (map:TiledMap, layerName:String):Null<FlxTilemap> {
+        var layerData = map.getLayer(layerName);
+        if (layerData != null) {
+            var layer = new FlxTilemap();
+            layer.loadMapFromArray(cast(layerData, TiledTileLayer).tileArray, map.width, map.height,
+                AssetPaths.tiles__png, map.tileWidth, map.tileHeight, FlxTilemapAutoTiling.OFF, 1, 1, 1)
+                .setPosition(0, Y_MAP_OFFSET);
+
+            layer.useScaleHack = false;
+            add(layer);
+
+            return layer;
+        }
+        return null;
     }
 }
--- src/objects/Player.hx
+++ src/objects/Player.hx
@@ -1,23 +1,113 @@
 package objects;
 
+import flixel.FlxG;
 import flixel.FlxSprite;
+import flixel.math.FlxPoint;
+
+typedef HoldsObj = {
+    var left:Float;
+    var right:Float;
+    var up:Float;
+    var down:Float;
+}
 
 class Player extends FlxSprite {
+    static inline final GRAVITY_ACCEL = 800;
+    static inline final FLY_UP_ACCEL = -1000;
+    static inline final X_ACCEL = 500;
+    static inline final X_MAX_VEL = 200;
+    static inline final MAX_UP_VEL = 100;
+    static inline final MAX_FLOAT_DOWN_VEL = 100;
+    static inline final MAX_DIVE_DOWN_VEL = 300;
+
+    var lrVel:Int = 0;
+    var udVel:Int = 0;
+
+    var holds:HoldsObj = {
+        left: 0,
+        right: 0,
+        up: 0,
+        down: 0
+    };
+
     public function new (x:Int, y:Int, scene:PlayState) {
         super(x, y);
         makeGraphic(16, 8, 0xff0d2030);
         setSize(16, 8);
 
-        maxVelocity.set();
+        drag.set(100);
     }
 
     override public function update (elapsed:Float) {
         handleInputs(elapsed);
+        handleVelocity(elapsed);
 
         super.update(elapsed);
     }
 
+    function handleVelocity (elapsed:Float) {
+        var yAccel = GRAVITY_ACCEL;
+        if (udVel == -1) {
+            yAccel += FLY_UP_ACCEL;
+        }
+
+        acceleration.set(X_ACCEL * lrVel, yAccel);
+        maxVelocity.set(
+            X_MAX_VEL,
+            velocity.y < 0
+                ? MAX_UP_VEL
+                : udVel == 1
+                    ? MAX_DIVE_DOWN_VEL
+                    : MAX_FLOAT_DOWN_VEL
+        );
+    }
+
     function handleInputs (elapsed:Float) {
-        if (true) {}
+        var controlsPressed = {
+            left: FlxG.keys.pressed.LEFT,
+            right: FlxG.keys.pressed.RIGHT,
+            up: FlxG.keys.pressed.UP,
+            down: FlxG.keys.pressed.DOWN
+        }
+
+        lrVel = 0;
+        udVel = 0;
+
+        if (controlsPressed.left) {
+            lrVel = -1;
+            holds.left += elapsed;
+        }
+
+        if (controlsPressed.right) {
+            lrVel = 1;
+            holds.right += elapsed;
+        }
+
+        if (controlsPressed.left && controlsPressed.right) {
+            if (holds.right > holds.left) {
+                lrVel = -1;
+            } else {
+                lrVel = 1;
+            }
+        }
+
+        if (controlsPressed.up) {
+            udVel = -1;
+            holds.up += elapsed;
+        }
+
+        if (controlsPressed.down) {
+            udVel = 1;
+            holds.down += elapsed;
+        }
+
+        if (controlsPressed.up && controlsPressed.down) {
+            if (holds.down > holds.up) {
+                udVel = -1;
+            } else {
+                udVel = 1;
+            }
+        }
+
     }
 }
